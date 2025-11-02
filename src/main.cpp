ebcd60b5231f5772efe4f5ca24da2929ecad33cc
Vhodron <ironali05@gmail.com>
2025-11-02 19:52:48 +0300

    Test Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "app.hpp"
 #include <QApplication>
 #include "QTHandler.hpp"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     QTHandler::instance();
     Controller controller;
+    cout << "Application started." << endl;
     return app.exec();
 }
